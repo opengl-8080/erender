7e3328d4c2ef600449c3dfd5bc198fb13fd108ca
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-05-27 00:06:10 +0900

    とりあえずHelloWorld

---
+++ src/main/kotlin/moeren/downloader/Main.kt
@@ -0,0 +1,19 @@
+package moeren.downloader
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Main::class.java, *args).use { context ->
+        val m = context.getBean(Main::class.java)
+        m.execute()
+    }
+}
+
+@SpringBootApplication
+open class Main {
+    
+    fun execute() {
+        
+    }
+}
